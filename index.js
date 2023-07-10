ae8c178f307c8ba12335ddbd5d76396ec5482327
John Carges <johncarges@gmail.com>
2023-07-10 14:59:42 -0400

    does some neat math

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+
+// Task 1
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+// Task 2
+let random = Math.floor(Math.random() * 10)
+
+// Task 3
+let num3 = 31;
+let num4 = 9;
+
+let mod = 31 % 9;
+
+// Task 4
+
+
+let max = Math.max(1,4,20)
+
+/*
+Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+Create a variable called random that will generate a random integer greater than 0.
+Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
+*/
